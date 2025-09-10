686acdccdfe6a5a9ed88905a60a27cbc19c54629
CyrilP <5814027+CyrilP@users.noreply.github.com>
2025-09-10 17:34:28 +0200

    parent 77aa267d82e91ca13d0591f6942dda5532a865ce author CyrilP <5814027+CyrilP@users.noreply.github.com> 1757518468 +0200 committer CyrilP <cyrilp@github.com> 1757664752 +0200
    parent 77aa267d82e91ca13d0591f6942dda5532a865ce
    author CyrilP <5814027+CyrilP@users.noreply.github.com> 1757518468 +0200
    committer CyrilP <cyrilp@github.com> 1757664694 +0200
    
    init
    
    init
    
    add requirements
    
    update requirements
    
    ruff
    
    Bump actions/checkout from 4.1.6 to 5.0.0
    
    Bumps [actions/checkout](https://github.com/actions/checkout) from 4.1.6 to 5.0.0.
    - [Release notes](https://github.com/actions/checkout/releases)
    - [Changelog](https://github.com/actions/checkout/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/actions/checkout/compare/v4.1.6...v5.0.0)
    
    ---
    updated-dependencies:
    - dependency-name: actions/checkout
      dependency-version: 5.0.0
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Bump actions/setup-python from 5.1.0 to 6.0.0
    
    Bumps [actions/setup-python](https://github.com/actions/setup-python) from 5.1.0 to 6.0.0.
    - [Release notes](https://github.com/actions/setup-python/releases)
    - [Commits](https://github.com/actions/setup-python/compare/v5.1.0...v6.0.0)
    
    ---
    updated-dependencies:
    - dependency-name: actions/setup-python
      dependency-version: 6.0.0
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Bump softprops/action-gh-release from 2.0.5 to 2.3.3
    
    Bumps [softprops/action-gh-release](https://github.com/softprops/action-gh-release) from 2.0.5 to 2.3.3.
    - [Release notes](https://github.com/softprops/action-gh-release/releases)
    - [Changelog](https://github.com/softprops/action-gh-release/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/softprops/action-gh-release/compare/v2.0.5...v2.3.3)
    
    ---
    updated-dependencies:
    - dependency-name: softprops/action-gh-release
      dependency-version: 2.3.3
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

---
+++ custom_components/enki/api.py
@@ -0,0 +1,214 @@
+"""API Placeholder.
+
+You should create your api seperately and have it hosted on PYPI.  This is included here for the sole purpose
+of making this example code executable.
+"""
+import aiohttp
+from dataclasses import dataclass
+from typing import Any
+
+from .const import (
+    LOGGER,
+    ENKI_OIDC_URL,
+    ENKI_URL,
+    ENKI_HOME_API_KEY,
+    ENKI_BFF_API_KEY,
+    ENKI_NODE_API_KEY,
+    ENKI_REFERENTIEL_API_KEY,
+    ENKI_USER_API_KEY)
+
+proxy = None
+
+@dataclass
+class Device:
+    """API device."""
+    home_id: str
+    device_id: str
+    node_id: str
+    device_name: str
+
+class API:
+    """Class for example API."""
+
+    def __init__(self, user: str, pwd: str) -> None:
+        """Initialise."""
+        self.user = user
+        self.pwd = pwd
+        self.connected: bool = False
+
+    @property
+    def controller_name(self) -> str:
+        """Return the name of the controller."""
+        return self.user
+
+    async def connect(self) -> bool:
+        """Connect to the Enki API."""
+        try:
+            async with aiohttp.ClientSession() as session, session.request(
+                method="POST",
+                url=ENKI_OIDC_URL,
+                headers={"Content-Type": "application/x-www-form-urlencoded"},
+                data={"grant_type":"password",
+                    "client_id": "enki-front",
+                    "username": self.user,
+                    "password": self.pwd},
+                proxy=proxy,) as resp:
+
+                    if resp.status == 200:
+                        response = await resp.json()
+                        LOGGER.debug("connect : " + str(response))
+                        self._access_token = response["access_token"]
+                        self._refresh_token = response["refresh_token"]
+                        self._token_type = response["token_type"]
+                        self.connected = True
+                        return True
+                    else:
+                        raise APIAuthError("Error connecting to api. Invalid username or password.")
+        except Exception as e:
+            raise APIConnectionError("Error connecting to api : " + repr(e))
+
+# *******************************************************
+    async def get_homes(self):
+        """Get list of homes."""
+        homes = []
+        async with aiohttp.ClientSession() as session, session.request(
+             method="GET",
+             url=f"{ENKI_URL}/api-enki-home-prod/v1/homes",
+             headers={"Authorization": f"{self._token_type} {self._access_token}",
+                      "X-Gateway-APIKey": ENKI_HOME_API_KEY},
+             proxy=proxy,) as resp:
+
+                if resp.status == 200:
+                    response = await resp.json()
+                    LOGGER.debug("get_homes : " + str(response))
+                    for home in response["items"]:
+                        homes.append(home["id"])
+                    return homes
+                else:
+                    raise ValueError("bad credentials")
+
+    async def get_home(self, home_id):
+        """Get details on home."""
+        async with aiohttp.ClientSession() as session, session.request(
+             method="GET",
+             url=f"{ENKI_URL}/api-enki-home-prod/v1/homes/{home_id}",
+             headers={"Authorization": f"{self._token_type} {self._access_token}",
+                      "X-Gateway-APIKey": ENKI_HOME_API_KEY},
+             proxy=proxy,) as resp:
+
+              if resp.status == 200:
+                  response = await resp.json()
+                  LOGGER.debug("get_home : " + str(response))
+              else:
+                raise ValueError("bad credentials")
+
+    async def get_items_in_section_for_home(self, home_id) -> list[dict[str, Any]]:
+            """Get sections in home."""
+            async with aiohttp.ClientSession() as session, session.request(
+             method="GET",
+             url=f"{ENKI_URL}/api-enki-mobile-bff-prod/v1/dashboard/homes/{home_id}?hasGroups=true",
+             headers={"Authorization": f"{self._token_type} {self._access_token}",
+                      "X-Gateway-APIKey": ENKI_BFF_API_KEY},
+             proxy=proxy,) as resp:
+                devices = []
+                if resp.status == 200:
+                    response = await resp.json()
+                    LOGGER.debug("get_items_in_section_for_home : " + str(response))
+                    for section in response["sections"]:
+                        for item in section["items"]:
+                            device = {
+                                "homeId": home_id,
+                                "deviceId": item["metadata"]["deviceId"],
+                                "nodeId": item["metadata"]["nodeId"],
+                                "deviceName": item["title"]["label"],
+                                "state": item["state"],
+                                "isEnabled": item["isEnabled"]
+                            }
+                            devices.append(device)
+
+                            node_info = await self.get_node(home_id, device.get("nodeId"))
+                            for info in node_info:
+                                if info != "id":
+                                    device[info] = node_info[info]
+                            device_info = await self.get_device(device.get("deviceId"))
+                            for info in device_info:
+                                if info != "id":
+                                    device[info] = device_info[info]
+                            LOGGER.debug("device : " + repr(device))
+                    return devices
+                  
+                else:
+                    raise ValueError("bad credentials")
+
+    async def get_node(self, home_id, node_id):
+        """Get details on a node."""
+        async with aiohttp.ClientSession() as session, session.request(
+             method="GET",
+             url=f"{ENKI_URL}/api-enki-node-agg-prod/v1/nodes/{node_id}",
+             headers={"Authorization": f"{self._token_type} {self._access_token}",
+                      "X-Gateway-APIKey": ENKI_NODE_API_KEY,
+                      "homeId": f"{home_id}"},
+             proxy=proxy,) as resp:
+
+                if resp.status == 200:
+                    response = await resp.json()
+                    LOGGER.debug("get_node : " + str(response))
+                    #print("\t\t" + response["icon"] + " " + response["factoryId"] + " " + response["modelNumber"])
+                    return response
+
+                else:
+                    raise ValueError("bad credentials")
+
+    async def get_device(self, id):
+        """Get details on a device."""
+        async with aiohttp.ClientSession() as session, session.request(
+             method="GET",
+             url=f"{ENKI_URL}/api-enki-referentiel-agg-prod/v1/devices/{id}?version=2.15.0",
+             headers={"Authorization": f"{self._token_type} {self._access_token}",
+                      "X-Gateway-APIKey": ENKI_REFERENTIEL_API_KEY},
+             proxy=proxy,) as resp:
+
+                if resp.status == 200:
+                    response = await resp.json()
+                    LOGGER.debug("get_device : " + str(response))
+                    return response
+
+                else:
+                    raise ValueError("bad credentials")
+
+    async def get_current_user(self):
+         """Get details on the current user."""
+         async with aiohttp.ClientSession() as session, session.request(
+             method="GET",
+             url=f"{ENKI_URL}/api-enki-user-prod/v1/users/current",
+             headers={"Authorization": f"{self._token_type} {self._access_token}",
+                      "X-Gateway-APIKey": ENKI_USER_API_KEY},
+             proxy=proxy,) as resp:
+
+                if resp.status == 200:
+                    response = await resp.json()
+                    LOGGER.debug(response)
+                else:
+                    raise ValueError("bad credentials")
+
+# *******************************************************
+
+    def disconnect(self) -> bool:
+        """Disconnect from api."""
+        self.connected = False
+        return True
+
+    async def get_devices(self) -> list[dict[str, Any]]:
+        """Get devices on api."""
+        homes = await self.get_homes()
+        devices = []
+        for home in homes:
+            devices.extend(await self.get_items_in_section_for_home(home))
+
+        return devices
+
+class APIAuthError(Exception):
+    """Exception class for auth error."""
+
+class APIConnectionError(Exception):
+    """Exception class for connection error."""
---
+++ custom_components/enki/light.py
@@ -0,0 +1,176 @@
+"""Light setup for our Integration."""
+
+from datetime import timedelta
+from typing import Any
+
+from homeassistant.components.light import ATTR_BRIGHTNESS, ColorMode, LightEntity
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from . import MyConfigEntry
+from .base import ExampleBaseEntity
+from .coordinator import ExampleCoordinator
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: MyConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+):
+    """Set up the Binary Sensors."""
+    # This gets the data update coordinator from the config entry runtime data as specified in your __init__.py
+    coordinator: ExampleCoordinator = config_entry.runtime_data.coordinator
+
+    # ----------------------------------------------------------------------------
+    # Here we are going to add some lights entities for the lights in our mock data.
+    # We have an on/off light and a dimmable light in our mock data, so add each
+    # specific class based on the light type.
+    # ----------------------------------------------------------------------------
+    lights = []
+
+    # On/Off lights
+    lights.extend(
+        [
+            ExampleOnOffLight(coordinator, device, "state")
+            for device in coordinator.data
+            if device.get("type") == "ON_OFF_LIGHT"
+        ]
+    )
+
+    # Dimmable lights
+    lights.extend(
+        [
+            ExampleDimmableLight(coordinator, device, "state")
+            for device in coordinator.data
+            if device.get("type") == "DIMMABLE_LIGHT"
+        ]
+    )
+
+    lights.extend(
+        [
+            EnkiLight(coordinator, device, "state")
+            for device in coordinator.data
+            if device.get("type") == "lights"
+        ]
+    )
+
+    # Create the lights.
+    async_add_entities(lights)
+
+class EnkiLight(ExampleBaseEntity, LightEntity):
+    """Implementation of an light depending on its capabilities."""
+    _attr_supported_color_modes = set()
+    _attr_color_mode = None
+
+    def __init__(
+        self, coordinator: ExampleCoordinator, device: dict[str, Any], parameter: str
+    ) -> None:
+        """Initialise entity."""
+        super().__init__(coordinator, device, parameter)
+        if "switch_electrical_power" in  device["capabilities"]:
+            self._attr_supported_color_modes.add(ColorMode.ONOFF)
+            self._attr_color_mode = ColorMode.ONOFF
+        
+        if "change_color_temperature" in device["capabilities"]:
+            self._attr_supported_color_modes.add(ColorMode.COLOR_TEMP)
+            self._attr_color_mode = ColorMode.COLOR_TEMP
+
+        if "change_brightness" in device["capabilities"]:
+            self._attr_supported_color_modes.add(ColorMode.BRIGHTNESS)
+            self._attr_color_mode = ColorMode.BRIGHTNESS
+
+class ExampleOnOffLight(ExampleBaseEntity, LightEntity):
+    """Implementation of an on/off light.
+
+    This inherits our ExampleBaseEntity to set common properties.
+    See base.py for this class.
+
+    https://developers.home-assistant.io/docs/core/entity/light/
+    """
+
+    _attr_supported_color_modes = {ColorMode.ONOFF}
+    _attr_color_mode = ColorMode.ONOFF
+
+    @property
+    def is_on(self) -> bool | None:
+        """Return if the binary sensor is on."""
+        # This needs to enumerate to true or false
+        return (
+            self.coordinator.get_device_parameter(self.device_id, self.parameter)
+            == "ON"
+        )
+
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        """Turn the entity on."""
+        await self.hass.async_add_executor_job(
+            self.coordinator.api.set_data, self.device_id, self.parameter, "ON"
+        )
+        # ----------------------------------------------------------------------------
+        # Use async_refresh on the DataUpdateCoordinator to perform immediate update.
+        # Using self.async_update or self.coordinator.async_request_refresh may delay update due
+        # to trying to batch requests.
+        # ----------------------------------------------------------------------------
+        await self.coordinator.async_refresh()
+
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        """Turn the entity off."""
+        await self.hass.async_add_executor_job(
+            self.coordinator.api.set_data, self.device_id, self.parameter, "OFF"
+        )
+        # ----------------------------------------------------------------------------
+        # Use async_refresh on the DataUpdateCoordinator to perform immediate update.
+        # Using self.async_update or self.coordinator.async_request_refresh may delay update due
+        # to trying to batch requests.
+        # ----------------------------------------------------------------------------
+        await self.coordinator.async_refresh()
+
+    async def async_set_off_timer(self, off_time: timedelta) -> None:
+        """Handle the set off timer service call.
+
+        Important here to have your service parameters included in your
+        function as they are passed as named parameters.
+        """
+        await self.hass.async_add_executor_job(
+            self.coordinator.api.set_data,
+            self.device_id,
+            "off_timer",
+            ":".join(str(off_time).split(":")[:2]),
+        )
+        # We have made a change to our device, so call a refresh to get updated data.
+        # We use async_request_refresh here to batch the updates in case you select
+        # multiple entities.
+        await self.coordinator.async_request_refresh()
+
+
+class ExampleDimmableLight(ExampleOnOffLight):
+    """Implementation of a dimmable light."""
+
+    _attr_supported_color_modes = {ColorMode.BRIGHTNESS}
+    _attr_color_mode = ColorMode.BRIGHTNESS
+
+    @property
+    def brightness(self) -> int:
+        """Return the brightness of this light between 0..255."""
+        # Our light is in range 0..100, so convert
+        return int(
+            self.coordinator.get_device_parameter(self.device_id, "brightness")
+            * (255 / 100)
+        )
+
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        """Turn the entity on."""
+        if ATTR_BRIGHTNESS in kwargs:
+            brightness = int(kwargs[ATTR_BRIGHTNESS] * (100 / 255))
+            await self.hass.async_add_executor_job(
+                self.coordinator.api.set_data, self.device_id, "brightness", brightness
+            )
+        else:
+            await self.hass.async_add_executor_job(
+                self.coordinator.api.set_data, self.device_id, self.parameter, "ON"
+            )
+        # ----------------------------------------------------------------------------
+        # Use async_refresh on the DataUpdateCoordinator to perform immediate update.
+        # Using self.async_update or self.coordinator.async_request_refresh may delay update due
+        # to trying to batch requests and cause wierd UI behaviour.
+        # ----------------------------------------------------------------------------
+        await self.coordinator.async_refresh()
